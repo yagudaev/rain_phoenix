fd7f3bde0eea248efe984c050074315a270fdd93
Michael Yagudaev <michaelyagudaev@gmail.com>
2020-05-29 21:12:40 -0700

    add os monitoring

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule PhoenixOrderForm.MixProject do
   def application do
     [
       mod: {PhoenixOrderForm.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
