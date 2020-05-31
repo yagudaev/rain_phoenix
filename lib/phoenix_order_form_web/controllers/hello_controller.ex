6635619cb68254e92f29712bf4802625ecfe05b6
Michael Yagudaev <michaelyagudaev@gmail.com>
2020-05-30 18:18:06 -0700

    throw out unneeded test controller

--- lib/phoenix_order_form_web/controllers/hello_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule PhoenixOrderFormWeb.HelloController do
-  use PhoenixOrderFormWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, :index)
-  end
-end
--- lib/phoenix_order_form_web/views/hello_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule PhoenixOrderFormWeb.HelloView do
-  use PhoenixOrderFormWeb, :view
-end
